5cf4e0f3aba2a7a4e8328c9952ae878a0e6ef1d2
Thomas Gosman <139735626+TGOSMAN@users.noreply.github.com>
2025-06-16 17:54:30 +1000

    Setup Lab2 Files

--- Labs/Lab1/Part5_Intro_Lab/main.cpp
+++ Labs/Lab1/Part5_Intro_Lab/main.cpp
@@ -6,14 +6,12 @@
 
 
 int main(int argc, char* argv[]) {
-	
+	//sequence to open and close a particular image with the changes for lab1 
 	char* name = new char[64];
 	sscanf(argv[1], "%s", name);
 	io_image *sample = IO_ImageNew(name);
 	IO_ImageAdd(sample, 60);
 	IO_ImageWriteBMP(sample, name);
-	//sample.num_components += 1;
-	// printf("Sample Num Components %d\n", sample.num_components);
 	IO_ImageDelete(sample);
 	return 0;
 }
