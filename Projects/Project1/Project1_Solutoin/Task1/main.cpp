8a1ecad8814209550aed327651864b8e8048aa19
Thomas Gosman <139735626+TGOSMAN@users.noreply.github.com>
2025-06-23 09:45:01 +1000

    Setup Project 1 Executable Paths

---
+++ Projects/Project1/Project1_Solutoin/Task1/main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+
+int main(void) {
+
+	printf("\t\t\tHello Worlds\n");
+	return 0;
+}
---
+++ Projects/Project1/Project1_Solutoin/Task2/main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+
+int main(void) {
+	printf("What a beautiful Day in The Neighbourhood\n");
+	return 0;
+
+}
